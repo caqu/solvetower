55ed84541e11aaec5e593ad686a15a586340a6cc
Carlos Quesada <quesadac@ae.com>
2015-07-13 23:47:49 -0400

    step 1

--- app.js
+++ app.js
@@ -1,75 +1,61 @@
+var Combinatorics = require('js-combinatorics');
 
-var space = [0,0,0,0,0];
+(function PuzzleSolver () {
 
-var circles = [(3).toString(2), (5).toString(2), (9).toString(2), (17).toString(2), (33).toString(2)];
+  var space = new Array(0,0,0,0,0);
 
-var pieces = [];
+  // Inner Circles: can only place one per layer
+  var circles = [
+                  [100001],
+                  [10001],
+                  [1001],
+                  [101],
+                  [11]
+                ];
+    //[(3).toString(2), (5).toString(2), (9).toString(2), (17).toString(2), (33).toString(2)];
 
-function solvePuzzle () {
+  // Outer shapes
+  var pieces = [
+                  [11111],
+                  [1,1111],
+                  [11,111],
+                  [10,1111],
+                  [101,111],
+                  [11,1110],
+                  [1,11,110],
+                  [11,10,110],
+                  [10,10,111],
+                  [10,111,100]
+                ];
 
-  // for each configuration of circles
-  {
-    // for each sort of pieces 
-      // for each vertical shift
-        // for each horizontal shift
-          // add piece
-          // test
-            // fail exit
+  function solvePuzzle (space, circles, pieces) {
+    // return console.log(typeof space);
+    s = space.slice(0);
+    c = circles.slice(0);
+    p = pieces.slice(0);
+
+    // Calculate all the ways in which the center circles can be arranged
+    positionedCircles = Combinatorics.permutationCombination(c);
+    
+    // TEMPORARILY SKIP COMBINATIONS LOWER THAN 5
+    positionedCircles = positionedCircles.toArray().slice(205);
+
+    positionedCircles.forEach(function(stack){
+      console.log(stack);
+    });
+
+    // for each configuration of circles
+    {
+      // for each sort of pieces 
+        // for each vertical shift
+          // for each horizontal shift
+            // add piece
+            // test
+              // fail exit
+    }
+    // TODO test it's upside-down version
   }
-}
-/* 
 
-// Inner Circles
+  solvePuzzle(space, circles, pieces);
 
-100001000000
-
-100010000000
-
-100100000000
-
-101000000000
-
-110000000000
-
-// Outer shapes
-
-000000000000
-101000000000
-111000000000
-
-011000000000
-010000000000
-110000000000
-
-010000000000
-010000000000
-111000000000
-
-000000000000
-000000000000
-111110000000
-
-000000000000
-000100000000
-111100000000
-
-001000000000
-011000000000
-110000000000
-
-000000000000
-001100000000
-111000000000
-
-010000000000
-111000000000
-100000000000
-
-000000000000
-001000000000
-111100000000
-
-000000000000
-011000000000
-111000000000
-*/
+})();
